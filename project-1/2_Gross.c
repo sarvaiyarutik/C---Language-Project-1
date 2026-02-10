825d9724541cf81ebff66e128d94b7bf2d69bc72
Sarvaiya Rutik <sarvaiyarutik18@gmail.com>
2026-02-10 12:07:02 +0530

    Refactor salary calculation and variable names

--- project-1/2_Gross.c
+++ project-1/2_Gross.c
@@ -2,29 +2,31 @@
 
 
 
+
+
+
 #include<stdio.h>
 
 int main(){
-   
-    float gross,salary,HRA,DA,TA;
-     
-    printf("Base salary gross : ");
-    scanf("%f",&gross);
+    float baseSalary, grossSalary,HRA,DA,TA ;
 
-    printf("Base salary HRA : ");
+    printf("Base salary :");
+    scanf("%f",&baseSalary);
+
+    printf("Enter your HRA :");
     scanf("%f",&HRA);
 
-    printf("Base salary DA : ");
+    printf("Enter your DA : ");
     scanf("%f",&DA);
 
-    printf("Base salary TA : ");
+    printf("Gross salary TA :");
     scanf("%f",&TA);
 
-    salary = gross +(gross * HRA / 100) + (gross * DA / 100) + (gross * TA / 100);
 
-    printf("Gross salary : Rs. %.2f",salary);
+    grossSalary = baseSalary + (baseSalary * HRA/100) + (baseSalary * DA / 100 ) + (baseSalary * TA / 100);
+
+
+    printf("Gross Salary : Rs %.1f", grossSalary);
 
     return 0;
-
-
 }
